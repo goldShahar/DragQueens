a449b3e6935002160da6ae6840ea15c4f28df2bf
AliceMager <mtmn06@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-11 12:53:53 +0000

    implemented read(), write() functions

---
+++ redili_manager/my_redis.py
@@ -0,0 +1,13 @@
+import redis
+from config import HOST, PORT, EXP
+
+POOL =  redis.ConnectionPool(host=HOST, port=PORT, decode_responses=True)
+
+def read(key: str):
+    my_server = redis.Redis(connection_pool=POOL)
+    response = my_server.get(key)
+    return response
+
+def write(key: str, value: any):
+    my_server = redis.Redis(connection_pool=POOL)
+    my_server.set(key, value,ex=EXP)
