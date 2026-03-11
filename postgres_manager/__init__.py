6dbacc2b93def47a2446cec346ceca9b9fd9b6d0
maayanamit <maayanamit222@gmail.com>
2026-03-11 11:07:51 +0000

    added files

---
+++ postgres_manager/__init__.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
+from sqlalchemy import create_engine, Enum
+from sqlalchemy import MetaData, DateTime
+
+class Base(DeclarativeBase):
+    pass
+
+
---
+++ postgres_manager/models/type.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class Type(BaseModel):
+    pass
