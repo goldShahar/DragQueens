711e33f820ae23d6b1b35de1675d362e8cdff866
yahel42 <yahelsayedoff@gmail.com>
2026-03-13 08:55:40 +0000

    type model so far

--- postgres_manager/models/type.py
+++ postgres_manager/models/type.py
@@ -7,7 +7,7 @@ class Type(BaseModel):
     Min_time: datetime 
     Min_area: float
 
-    def create_type(self):
+    def create_type(self): 
         if read_type(type_name=self.type_name, selected_columns=[True, False, False, False]):
             return "Type already exists"
         return create_type_in_pg(self.type_name, self.Importance, self.Min_time, self.Min_area)
@@ -15,11 +15,9 @@ class Type(BaseModel):
 
 
 def delete_type(type_name: str):
-    if not read_type(type_name=type_name, selected_columns=[True, False, False, False]):
-        return "Type does not exist"
     return delete_type_in_pg(type_name)
 
 
 def read_type(type_name: str | None, Importance: int | None, Min_time: datetime | None, Min_area: float | None, selected_columns: list[bool]):
-    return read_type_from_pg([type_name, Importance, Min_time, Min_area], selected_columns)
+    return read_type_from_pg({"type_name": type_name, "Importance": Importance, "Min_time": Min_time, "Min_area": Min_area}, selected_columns)
 
