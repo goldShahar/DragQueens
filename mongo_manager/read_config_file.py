3e68ee8e347994ce3c83e45f657ad6183ede662e
goldShahar <shahar.gold.sun@gmail.com>
2026-03-13 13:22:20 +0000

    changed config file in mongo manager

--- mongo_manager/read_config_file.py
+++ mongo_manager/read_config_file.py
@@ -9,4 +9,4 @@ def read_config_file(path: str) -> dict:
         raise e
 
 
-mongo_config = read_config_file("mongo_manager/config.json")
+mongo_config = read_config_file("mongo_manager/mongo_config.json")
