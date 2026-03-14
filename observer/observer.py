3a82c1ecc25ca008c6d5f86b1597259f62e3fe25
goldShahar <shahar.gold.sun@gmail.com>
2026-03-14 14:33:13 +0000

    observer works

---
+++ observer/observer.py
@@ -0,0 +1,9 @@
+from redili_manager import my_redis
+
+
+def get_message_by_id(id: str):
+    status = my_redis.read(id)
+    if status:
+        return status
+    else:
+        return "This id does not exits or this reqest was handaled a while back and no longer in our system"
