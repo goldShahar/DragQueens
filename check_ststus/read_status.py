c85f575bba5ece5e41e36fd786d2a63c96975061
goldShahar <shahar.gold.sun@gmail.com>
2026-03-14 17:42:40 +0000

    check status api works

---
+++ check_ststus/read_status.py
@@ -0,0 +1,20 @@
+from observer.observer import get_message_by_id
+from config import HOST, PORT
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/message_status")
+def get_message_status(message_id: str):
+    return get_message_by_id(message_id)
+
+
+def start_server():
+    uvicorn.run(app, host=HOST, port=PORT)
+
+
+if __name__ == "__main__":
+    print("start")
+    start_server()
