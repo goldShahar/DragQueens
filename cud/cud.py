42d9d9e3a1547d1f94752a045999d21e39e617de
AliceMager <mtmn06@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-15 14:59:09 +0000

    implemented cud functions

---
+++ cud/cud.py
@@ -0,0 +1,41 @@
+import uuid
+from kafkush.producers.producer import send_to_kafka
+from config import TOPIC1, INSERT,  FAILURE, TYPE
+from validator import insert_type_validation, delete_type_validation, existing_type_validator
+
+def insert_type(hazard_type: dict):
+    if insert_type_validation(hazard_type):
+        send_to_kafka(hazard_type, TOPIC1) 
+        return hazard_type['id_msg']
+    return FAILURE
+
+def delete_type(hazard_type: dict):
+    if delete_type_validation(hazard_type['type_name']):
+        send_to_kafka(hazard_type, TOPIC1)
+        return hazard_type['id_msg']
+    return FAILURE
+
+
+def insert_hazard(hazard: dict):
+    if existing_type_validator(hazard['type_name']):
+        send_to_kafka(hazard, TOPIC1)
+        return hazard['id_msg']
+    return FAILURE
+
+def delete_hazard(hazard: dict):
+   send_to_kafka(hazard, TOPIC1)
+   return hazard['id_msg']
+
+
+def final_func(values: dict, action: str, class_type: str):
+    values['id_msg'] = uuid.uuid4() 
+    if class_type == TYPE:
+        if action == INSERT:
+            return insert_type(values)
+        else:
+            return delete_type(values)
+    else:
+        if action == INSERT:
+            return insert_hazard(values)
+        else:
+            return delete_hazard(values)
