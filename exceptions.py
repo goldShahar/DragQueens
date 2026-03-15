acfad9bf2c49c8c97732f7950d4a25a21d64d835
AliceMager <mtmn06@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-15 14:56:59 +0000

    deleted exceptions.py

--- exceptions.py
+++
@@ -1,9 +0,0 @@
-class RedisError(Exception):
-    def __init__(self, message: str, status_code: int = 400):
-        super().__init__(message)
-        self.message = message
-        self.status_code = status_code
-
-class KeyNotFound(RedisError):
-    def __init__(self, message="The provided key doesn't exist in redis."):
-        super().__init__(message,status_code=400)
