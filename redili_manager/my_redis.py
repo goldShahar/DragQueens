84fa73d5429ea4c02a54832247a38a2955d42ee8
AliceMager <mtmn06@matmon-development-machine.xmeg0jdevaee3lh03vzpt13orh.tlvx.internal.cloudapp.net>
2026-03-15 10:10:48 +0000

    added delete_key() function in redis

--- redili_manager/my_redis.py
+++ redili_manager/my_redis.py
@@ -1,5 +1,6 @@
 import redis
 from config import HOST, REDIS_PORT, EXP
+from exceptions import KeyNotFound
 
 POOL =  redis.ConnectionPool(host=HOST, port=REDIS_PORT, decode_responses=True)
 
@@ -11,3 +12,10 @@ def read(key: str):
 def write(key: str, value: any):
     my_server = redis.Redis(connection_pool=POOL)
     my_server.set(key, value,ex=EXP)
+
+def delete_key(key: str):
+    my_server = redis.Redis(connection_pool=POOL)
+    if my_server.get(key):
+        my_server.delete(key)
+    else:
+        raise KeyNotFound
