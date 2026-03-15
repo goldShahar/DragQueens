d9a4b12066e3e9204cdb3e153178d5aab1527af4
goldShahar <shahar.gold.sun@gmail.com>
2026-03-15 15:17:04 +0000

    added router for hazard table

---
+++ http_writer/routers/hazard.py
@@ -0,0 +1,37 @@
+from config import INSERT, DELETE, HAZARD
+from cud.cud import final_func
+
+from datetime import datetime
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/hazard")
+
+
+@router.get("/create")
+def create_hazard(
+    hazerd_type: str,
+    hazerd_id: str,
+    goe_polygon,
+    start_time: datetime,
+    end_time: datetime,
+):
+    final_func(
+        {
+            "type": hazerd_type,
+            "id": hazerd_id,
+            "goe_polygon": goe_polygon,
+            "start_time": start_time,
+            "end_time": end_time,
+        },
+        INSERT,
+        HAZARD,
+    )
+
+
+@router.get("/delete")
+def delete_hazard(hazerd_id: str, hazerd_type: str | None):
+    final_func(
+        {"type": hazerd_type, "id": hazerd_id},
+        DELETE,
+        HAZARD,
+    )
