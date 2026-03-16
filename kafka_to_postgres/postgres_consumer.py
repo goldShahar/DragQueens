7b6cf4d30827cdddbbd2d7ff752ec33329d0020b
goldShahar <shahar.gold.sun@gmail.com>
2026-03-16 08:40:42 +0000

    fully conected posgress manager to kafka, can read and write

--- kafka_to_postgres/postgres_consumer.py
+++ kafka_to_postgres/postgres_consumer.py
@@ -1,11 +1,14 @@
+from postgres_manager.welcome_func import welcome_func
 from kafkush.consumers import consumer
+from kafkush.producers import send_to_kafka
 from config import TOPIC1
-
-
-def welcome_func(action: str, table: str, valuses: dict[str, any]):
-    pass
 
 
 def write_topic1_to_postgress_func():
     status = consumer.read_from_kafka(TOPIC1)
-    welcome_func(status["ACTION"], status["TABLE"], status["VALUSES"])
+    answer = welcome_func(status["ACTION"], status["TABLE"], status["VALUSES"])
+    write_postgress_to_topic1_func(answer)
+
+
+def write_postgress_to_topic1_func(answer):
+    send_to_kafka(answer, TOPIC1)
