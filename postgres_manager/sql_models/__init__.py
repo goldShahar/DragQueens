a0632d9b9a8d2b3c6b8dc48d8ea8338fec17ece8
yahel42 <yahelsayedoff@gmail.com>
2026-03-16 15:10:30 +0000

    Insert, delete and more are wortking

--- postgres_manager/sql_models/__init__.py
+++ postgres_manager/sql_models/__init__.py
@@ -3,7 +3,6 @@ from geoalchemy2 import Geometry, WKBElement
 from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
 from sqlalchemy import ARRAY, String, create_engine, func
 from sqlalchemy import MetaData, DateTime
-from sqlalchemy.types import TypeDecorator
 postgres_con_url = "postgresql://{}:{}@{}/{}"
 
 
@@ -19,18 +18,6 @@ metadata = MetaData()
 
 class Base(DeclarativeBase):
     pass
-
-
-class TransformGeometry(TypeDecorator):
-    impl = Geometry
-
-    def __init__(self):
-        super().__init__(geometry_type="POLYGON", srid=4326)
-        self.srid = 4326
-        self.geometry_type = "POLYGON"
-
-    def column_expression(self, column):
-        return getattr(func, self.impl.asbinary)
 
 
 class Type(Base):
